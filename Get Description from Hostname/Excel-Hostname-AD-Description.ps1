166b8e74e5d7d240d07dbf4f4298926852bf7d62
Joseph Gonzales <jgonzales@kmha.com>
2020-05-21 14:12:59 -0400

    Restructured directories

---
+++ Get Description from Hostname/Excel-Hostname-AD-Description.ps1
@@ -0,0 +1,19 @@
+﻿
+$computers = ""
+
+ForEach ($hostname in $computers) {
+
+    try {
+    $description = Get-ADComputer $hostname -Properties Description | select -ExpandProperty description
+    }
+    catch {
+    $description = "No description"
+    }
+    if ( $description -eq $null) {
+        echo "No description"
+        }
+    else {
+        echo "$description"
+        }
+
+    }
